26b8691b38fa940d2c7a7723cf006d6f0989b530
xiaoyuej <xiaoyuej@welldeliver.cn>
2022-07-18 14:32:04 +0800

    创建文件

---
+++ blog_web/src/views/Home/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+import "./home.less";
+
+const Home = () => (
+  <div className="Home">
+    <div className="container">
+    222
+    </div>
+  </div>
+);
+
+export default Home;
