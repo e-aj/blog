07aa0cef6ba4e02683ef2f0d55eae93582675582
xiaoyuej <xiaoyuej@welldeliver.cn>
2022-08-26 15:23:06 +0800

    用户信息接口

--- blog_serve/router/userinfo.js
+++ blog_serve/router/userinfo.js
@@ -6,7 +6,10 @@ const router = express.Router()
 const userinfo_handler = require('../user_handler/userinfo')
 
 // 获取用户的基本信息
-router.post('/userinfo',userinfo_handler.getUserinfo)
+router.get('/userinfo',userinfo_handler.getUserinfo)
+
+// 更新用户信息
+router.post('userindo',userinfo_handler.postUserinfo)
 
 // 向外共享路由
 module.exports = router
--- blog_serve/user_handler/userinfo.js
+++ blog_serve/user_handler/userinfo.js
@@ -1,5 +1,31 @@
-const express = require('express')
+const db = require('../db/index')
 
 exports.getUserinfo = (req,res)=>{
-    res.send('pk')
+    const sql = `select id,username from blog_user where id=?`
+    db.query(sql,req.user.id,(err,result)=>{
+        if (err) return res.send({status:1,message:err.message})
+
+        if(result.length !== 1) return res.send({status:1,message:'获取用户信息失败！'})
+
+        res.send({
+            status:1,
+            message:'获取用户信息成功！',
+            data:result[0]
+        })
+    })
+}
+
+// 更新用户信息
+exports.postUserinfo = (req,res) =>{
+    const sql =    `update blog_user set ? where id=?`
+
+    db.query(sql,[req.body,req.body.id],(err,result) =>{
+        if (err) return res.send({status:1,message:err.message})
+
+        if(result.affectedRows !==1 ) return res.send({status:1,message:'修改用户信息失败！'})
+
+        // 成功
+        res.send({status:0,message:'修改用户信息成功！'})
+    })
+
 }
