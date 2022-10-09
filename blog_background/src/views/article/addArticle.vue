71c299cef062116c01e457eb343659cf506fc26d
xiaoyuej <xiaoyuej@welldeliver.cn>
2022-10-09 17:42:50 +0800

    添加页面优化

--- blog_background/src/views/article/addArticle.vue
+++ blog_background/src/views/article/addArticle.vue
@@ -10,15 +10,73 @@
         </router-link>
       </template>
     </a-breadcrumb>
+    <div class="container">
+      <a-form :model="articleData" :wrapper-col="{ span: 10 }">
+        <a-form-item has-feedback label="标题" name="title">
+          <a-input v-model:value="articleData.title" type="text" />
+        </a-form-item>
+        <a-form-item has-feedback label="封面" name="title">
+          <!-- <a-input v-model:value="articleData.title" type="text" /> -->
+          <input type="file" />
+        </a-form-item>
+        <a-form-item has-feedback label="分类" name="title">
+          <a-select
+            v-model:value="value"
+            style="width: 100%"
+            placeholder="请选择分类"
+            :options="cateOption.map((item) => ({ value: item.id, label: item.name }))"
+            @select="selectOk"
+          ></a-select>
+        </a-form-item>
+      </a-form>
+      <div style="border: 1px solid #ccc; margin-top: 10px">
+        <Toolbar
+          :editor="editorRef"
+          :defaultConfig="toolbarConfig"
+          :mode="mode"
+          style="border-bottom: 1px solid #ccc"
+        />
+        <Editor
+          :defaultConfig="editorConfig"
+          :mode="mode"
+          v-model="articleData.content"
+          style="height: 400px; overflow-y: hidden"
+          @onCreated="handleCreated"
+          @onChange="handleChange"
+          @onDestroyed="handleDestroyed"
+          @onFocus="handleFocus"
+          @onBlur="handleBlur"
+          @customAlert="customAlert"
+          @customPaste="customPaste"
+        />
+      </div>
+    </div>
+    <div class="save">
+      <a-button type="primary" @click="saveArticle">保存</a-button>
+      <a-button type="primary">取消</a-button>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import {
+  defineComponent,
+  ref,
+  onBeforeUnmount,
+  shallowRef,
+  onMounted,
+  reactive,
+} from "vue";
+import "@wangeditor/editor/dist/css/style.css"; // 引入 css
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+import { addArticle } from "../../api/article";
+import { message } from "ant-design-vue";
+import { getArticleCate } from "../../api/artCate";
 interface Route {
   path: string;
   breadcrumbName: string;
 }
 export default defineComponent({
+  components: { Editor, Toolbar },
   setup() {
     // 定义面包屑
     const routes = ref<Route[]>([
@@ -35,9 +93,140 @@ export default defineComponent({
         breadcrumbName: "添加文章",
       },
     ]);
+
+    // 编辑器实例，必须用 shallowRef，重要！
+    const editorRef = shallowRef();
+
+    // 内容 HTML
+    const valueHtml = ref("");
+
+    // 模拟 ajax 异步获取内容
+    onMounted(() => {
+      setTimeout(() => {
+        articleData.content = "";
+      }, 1500);
+    });
+
+    const toolbarConfig = {};
+    const editorConfig = { placeholder: "请输入内容..." };
+
+    // 组件销毁时，也及时销毁编辑器，重要！
+    onBeforeUnmount(() => {
+      const editor = editorRef.value;
+      if (editor == null) return;
+
+      editor.destroy();
+    });
+
+    // 编辑器回调函数
+    const handleCreated = (editor: any) => {
+      console.log("created", editor);
+      editorRef.value = editor; // 记录 editor 实例，重要！
+    };
+    const handleChange = (editor: any) => {
+      console.log("change:", editor.getHtml());
+    };
+    const handleDestroyed = (editor: any) => {
+      console.log("destroyed", editor);
+    };
+    const handleFocus = (editor: any) => {
+      console.log("focus", editor);
+    };
+    const handleBlur = (editor: any) => {
+      console.log("blur", editor);
+    };
+    const customAlert = (info: any, type: any) => {
+      alert(`【自定义提示】${type} - ${info}`);
+    };
+    const customPaste = (editor: any, event: any, callback: any) => {
+      console.log("ClipboardEvent 粘贴事件对象", event);
+
+      // 自定义插入内容
+      editor.insertText("xxx");
+
+      // 返回值（注意，vue 事件的返回值，不能用 return）
+      callback(false); // 返回 false ，阻止默认粘贴行为
+      // callback(true) // 返回 true ，继续默认的粘贴行为
+    };
+
+    const insertText = () => {
+      const editor = editorRef.value;
+      if (editor == null) return;
+
+      editor.insertText("hello world");
+    };
+
+    const printHtml = () => {
+      const editor = editorRef.value;
+      if (editor == null) return;
+      console.log(editor.getHtml());
+    };
+
+    const disable = () => {
+      const editor = editorRef.value;
+      if (editor == null) return;
+      editor.disable();
+    };
+
+    interface articleDataType {
+      title: string;
+      content: string;
+      cate_id: number;
+    }
+
+    // 文章信息
+    const articleData = reactive<articleDataType>({
+      title: "",
+      content: valueHtml.value,
+      cate_id: "",
+    });
+
+    // 分类信息
+    // const cateOption = reactive([]);
+    const cateOption = ref([]);
+    getArticleCate().then((res) => {
+      cateOption.value = res.data;
+    });
+
+    // 分类选择完成
+    const selectOk = (value: number) => {
+      articleData.cate_id = value;
+    };
+
+    // 保存
+    const saveArticle = () => {
+      console.log(articleData);
+      addArticle(articleData).then((res) => {
+        if (res.status === 0) {
+          message.success(res.message);
+        } else {
+          message.success(res.message);
+        }
+      });
+    };
+
     return {
       basePath: "",
       routes,
+      editorRef,
+      mode: "default",
+      valueHtml,
+      toolbarConfig,
+      editorConfig,
+      articleData,
+      cateOption,
+      handleCreated,
+      handleChange,
+      handleDestroyed,
+      handleFocus,
+      handleBlur,
+      customAlert,
+      customPaste,
+      insertText,
+      printHtml,
+      disable,
+      saveArticle,
+      selectOk,
     };
   },
 });
